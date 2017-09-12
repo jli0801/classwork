239a71edf1956250cdd0927fc266ac48e1c13fbd
BT_1N3_14 <BT_1N3_14@BT_1N3_14.bktech.local>
2017-09-12 14:50:24 -0400

    part 1

---
+++ intro/src/intro/CodingConvention.java
@@ -0,0 +1,18 @@
+package intro;
+
+public class CodingConvention {
+
+	/*
+	 * constructor is like method; return type = an instance of class a constructor must ALWAYS be named after 
+	the class 
+	*/
+	public CodingConvention() {
+		
+	}
+	
+	//normal method "void" = doesn't return anything
+	public void doStuff() {
+		
+		System.out.println("Hello world!");
+	}
+}
--- intro/src/intro/IntroMain.java
+++ intro/src/intro/IntroMain.java
@@ -1,6 +1,21 @@
-package intro;
+package intro; //package declaration: matches folder where the file is located
 
+//class header
 public class IntroMain {
-//lineoftext
-//anotherline
+
+	//first method executed. static because independent of instances 
+	public static void main(String[] args) {
+		//this is the declaration of an instance of CodingConvention
+		//it is being instantiated
+		//note the use of the word new; used when calling constructors
+		CodingConvention conventionInstance = new CodingConvention();
+		//a note on local variables: NOT FIELDS (HAS-A Relationship)
+		//after the method, they are destroyed
+		
+		
+		//instance method call (the method belongs to the instance not class) 
+		//depends on the individual
+		conventionInstance.doStuff();
+			
+	}
 }
