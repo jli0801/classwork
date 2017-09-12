cbc5c4c0cc12f7259c23de035e24d9cbe71721da
BT_1N3_14 <BT_1N3_14@BT_1N3_14.bktech.local>
2017-09-12 15:17:27 -0400

    part two

--- intro/src/intro/CodingConvention.java
+++ intro/src/intro/CodingConvention.java
@@ -1,18 +1,31 @@
 package intro;
 
 public class CodingConvention {
-
+	
+	//FIELDS ARE AT THE TOP!!
+	private String name; //DECLARE FIELDS ONLY, INSTANTIATE IN CONSTRUCTOR!
+	private String description; 
+	/* DONT DO THIS
+	 * private String name = "what";
+	*/
 	/*
 	 * constructor is like method; return type = an instance of class a constructor must ALWAYS be named after 
 	the class 
 	*/
-	public CodingConvention() {
+	public CodingConvention(String name, int descriptionIndex) {
+		//Instantiate variables now: (only in constructor) 
 		
+		//there are two variables called "name" the local variable and the field 
+		//distinguish between the two by using the reserved word "this" 
+		this.name = name;
+		
+		//this is a static call to a constant
+		description = IntroMain.DESCRIPTIONS[descriptionIndex];
 	}
 	
 	//normal method "void" = doesn't return anything
 	public void doStuff() {
-		
-		System.out.println("Hello world!");
+		String output = name + description;
+		System.out.println(output);
 	}
 }
--- intro/src/intro/IntroMain.java
+++ intro/src/intro/IntroMain.java
@@ -2,20 +2,27 @@ package intro; //package declaration: matches folder where the file is located
 
 //class header
 public class IntroMain {
+	
+	public static final String[] DESCRIPTIONS = {
+			" is a student at BTHS" , " is a teacher at BTHS"};
 
 	//first method executed. static because independent of instances 
 	public static void main(String[] args) {
 		//this is the declaration of an instance of CodingConvention
 		//it is being instantiated
 		//note the use of the word new; used when calling constructors
-		CodingConvention conventionInstance = new CodingConvention();
+		
+		int numberOfPeople = 10;
+		for(int i = 0; i < numberOfPeople; i++){
+			CodingConvention conventionInstance = new CodingConvention("Jessica", i%2);
+			conventionInstance.doStuff();
+		}
 		//a note on local variables: NOT FIELDS (HAS-A Relationship)
 		//after the method, they are destroyed
 		
 		
 		//instance method call (the method belongs to the instance not class) 
 		//depends on the individual
-		conventionInstance.doStuff();
-			
+	
 	}
 }
