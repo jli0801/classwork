e25d4300dbb195e37bbb25852651fecf3088fe9f
BT_1N3_14 <BT_1N3_14@BT_1N3_14.bktech.local>
2017-11-10 11:08:31 -0500

    classwork

---
+++ intro/src/javaTheHardWay/Environment.java
@@ -0,0 +1,16 @@
+package javaTheHardWay;
+
+public class Environment {
+
+	private int plants;
+	private int growthRate;
+	public Environment(int plants) {
+		this.plants = plants;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
