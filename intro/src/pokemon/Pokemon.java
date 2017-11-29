dd512f83493756fffe55c26f7e99fde9aec8c7b8
BT_1N3_14 <BT_1N3_14@BT_1N3_14.bktech.local>
2017-11-29 15:26:35 -0500

    Anonymous Inner Type

---
+++ intro/src/pokemon/Pokemon.java
@@ -0,0 +1,91 @@
+package pokemon;
+
+public class Pokemon {
+
+	private int level;
+	private int hp;
+	private String name;
+	private boolean poisoned;
+	public Pokemon(String name, int level) {
+	this.name = name;
+	this.level = level;
+	hp = 100;
+	poisoned  = false;
+	}
+
+	public static void main(String[] args) {
+		 Pokemon squirtle = new Pokemon("Squirtle",26);
+		 Pokemon bulbasaur = new Pokemon("Bulbasaur",26);
+		 squirtle.iChooseYou();
+		 bulbasaur.iChooseYou();
+		 
+		 squirtle.attack(bulbasaur, new Attack() {
+			 public void attack(Pokemon target)
+			 {
+			 int hp = target.getHp();
+			 target.setHp(hp/2);
+			 }
+		 });
+		 
+		 System.out.println("Bulbasaur is preparing to attack!");
+		 bulbasaur.attack(squirtle, new Attack() {
+			 public void attack(Pokemon target)
+			 {
+			target.setPoisoned(true);
+			 }
+		 });
+		 
+		 squirtle.lapse();
+		 bulbasaur.lapse();
+		 squirtle.attack(bulbasaur, new Attack() {
+			public void attack(Pokemon target) {
+				int hp = target.getHp();
+				 target.setHp(hp-100);
+				
+			}
+		});
+		//ctrl space 
+		 
+
+	}
+	private void iChooseYou()
+	{
+		System.out.println(name + ", " + name + "!");
+	}
+	
+
+	public String getName() {
+		return name;
+	}
+
+	public void setHp(int newHP) {
+		this.hp = newHP;
+	}
+
+	public void setPoisoned(boolean b) {
+		this.poisoned = poisoned;
+	}
+	public void lapse()
+	{
+		if(poisoned)
+		{
+			setHp(hp -15);
+		}
+	}
+
+	public int getHp() {
+		return hp;
+	}
+
+	public boolean isPoisoned() {
+		return poisoned;
+	}
+	 public void attack(Pokemon target, Attack attack){
+		 if(Math.random() < .9){
+		 attack.attack(target);
+		 System.out.println("The attack hit");
+		 }else{
+		 System.out.println("The attack missed");
+		 }
+		 }
+}
