83211f17f78bf5ac23c904ba535d906fc59780f6
BT_1N3_14 <BT_1N3_14@BT_1N3_14.bktech.local>
2017-12-04 15:22:47 -0500

    bug

--- intro/src/recursion/TowerHanoi.java
+++ intro/src/recursion/TowerHanoi.java
@@ -2,7 +2,7 @@ package recursion;
 
 public class TowerHanoi {
 
-	private int disks;
+	private static int disks;
 	private static String start;
 	private static String helper;
 	private static String end;
@@ -18,21 +18,22 @@ public class TowerHanoi {
 
 	public static void main(String[] args) {
 		
-		result = solve(3, start, helper, end);
+		result = solve(3, "start", "helper", "end");
 		System.out.println(result);
 	}
 
 	private static String solve(int n, String start, String helper, String end) {
 		if(n==1)
 		{
-			return start + " moves to " + end;
+			return start + " moves to " + end + "\n";
+			
 		}
 		else
 		{
 			String part1, part2, part3;
-			part1 = solve(n-1, start, helper, end);
+			part1 = solve(n-1, start, helper, end) + "\n";
 			part2 = start + " moves to " + end + "\n";
-			part3 = solve(n-1, helper, end, start);
+			part3 = solve(n-1, helper, end, start) + "\n";
 			return part1 + part2 + part3;
 		}
 		
